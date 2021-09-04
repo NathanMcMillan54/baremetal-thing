6b9373882ef6da5dd385a5b5c9053fdc0da88d11
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-09-04 14:10:52 -0400

    Added blink function from novuskinc.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,22 @@
 #![no_main]
 
 #[macro_use] extern crate novusk;
-use novusk::arm::libbmu::app_main;
+use novusk::arm::libbmu::{app_main, Time};
 use novusk::kernel::printk::printk;
+use novusk::novusk::led::blink;
 
 fn main() -> ! {
+    let mut time = Time::new();
+    time.sleepc(1500);
+
     printk!("\nHello, world!");
 
-    loop {  }
+    printk!("Blinking and looping for ever...");
+    loop {
+        blink(999999);
+    }
+
+    panic!("App shouldn't have ended");
 }
 
 app_main!(main);
